01d7a1051cb50b7ddb412ee2a926a09174d0106f
Blouse <nblous5177@greenville.k12.sc.us>
2017-09-15 09:14:12 -0400

    Added more helper functions

--- guess_a_number.py
+++ guess_a_number.py
@@ -10,21 +10,30 @@ print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
 tries = 0
+#helper functions
+def get_guess():
+    while True:
+        g = input("Take a guess: ")
+
+        if g.isnumeric():
+            g = int(g)
+            return g
+        else:
+            print("You must enter a number.")
 
 #play the game
 while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
+    guess = get_guess()
     
     if guess < rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-
-   tries += 1
     
+    tries += 1
+   
 #game over
 if tries == rand:
     print("You win!")
 else:
-    print("You're stupid and should just quit playing!")
+    print("You're stupid and should just quit playing! The real number was " + str(rand) + ".")
